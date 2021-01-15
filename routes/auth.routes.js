b1eac4f9d01c11cdfd3b92dd1ab58c5b12e367fc
alivadjid1 <alivadjid@gmail.com>
2021-01-15 17:23:09 +0300

    Регистрация роутов завершена. Добавлен bcrypt для шифрования пароля. Добавлен express-validator для проверки и добавлен middlewar. Добавлен jsonwebtoken для формирования веб-токена на 1 час

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,10 +3,30 @@ const {Router} = require('express')
 const User = require('../models/user')
 const router = Router()
 const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 // /api/auth/register
-router.post('/register', async(req, res)=> {
+router.post(
+  '/register',
+  // массив middlewar-ов для валидации данных из фронта
+  [
+    check('email', 'Некорректный email').isEmail(),
+    check('password', 'Минимальная длина пароля 6 симоволов')
+      .isLength({ min: 6})
+  ],
+  async(req, res)=> {
   try{
+    // обработка валидации
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Некорректные данные при регистрации'
+      })
+    }
+
     //получаем данные из фронт
     const { email, password } = req.body
 
@@ -32,8 +52,47 @@ router.post('/register', async(req, res)=> {
 })
 
 // /api/auth/login
-router.post('/register', async(req, res)=> {
+router.post(
+  '/register',
+  [
+    check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+    check('password', 'Введите пароль').exists() // exists - пароль просто существует
+  ],
+  async(req, res)=> {
+    try{
+      // обработка валидации
+      const errors = validationResult(req)
+      if (errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректные данные при входе в систему'
+        })
+      }
+      const { email, password } = req.body
 
+      const user = await User.findOne({ email })
+
+      if(!user) {
+        return res.status(400).json({message: 'Пользователь не найден'})
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if(!isMatch) {
+        return res.status(400).json({message:'Неверный пароль, попробуйте снова'})
+      }
+
+      const token = jwt.sign(
+        { userId: user.id }, // объект для шифрования
+        config.get('jwtSecret'),  //секретный ключ
+        { expiresIn: '1h'} //через сколько этот токен перестанет действовать
+      )
+
+      res.json({ token, userId: user.id })
+
+    } catch(e){
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'})
+    }
 })
 
 
