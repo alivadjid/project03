46b16167c896bf60081c2259498726dfe2f7b534
alivadjid1 <alivadjid@gmail.com>
2021-01-17 15:52:17 +0300

    Работа над бизнес логикой завершена. link.routes.js поправлен

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = (req, res, next) => {
+  if(req.method === 'OPTIONS') { //базовая проверка. спец. метод в rest API, кот-ый проверяет доступность сервера
+    return next()
+  }
+
+  try {
+    const token = req.headers.authorization.split('')[1] // "Bearer TOKEN"
+
+    if(!token) {
+      //если токена нет
+      return res.status(401).json({ message: 'Нет авторизации'})
+    }
+    //раскодировка токена
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decoded
+    next()
+  } catch(e) {
+      res.status(401).json({message: 'Нет авторизации'})
+  }
+}
--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,11 +1,38 @@
 const { Router } = require('express')
 const Link = require('../models/Link')
 const router  = Router()
+const auth = require('../middleware/auth.middleware')
+const config = require('../config')
+const shortid = require('shortid')
 
 //обработка нескольких запросов
 
-router.post('/generate', async(req,res) => {
+router.post('/generate', auth,async(req,res) => {
   try{
+    const baseUrl = config.get('baseUrl')
+
+    //с фронта получаем обеъкт from
+    const { from } = req.body
+
+    const code = shortid.generate()
+
+    //проверка в базе ссылки from
+
+    const existing = await Link.findOne({from})
+
+    if(existing) {
+      res.json({ link: existing})
+    }
+
+    const to = baseUrl + '/t' + code
+
+    const link = new Link({
+      code, to, from, owner: req.user.userId
+    })
+
+    await link.save()
+    //статус created
+    res.status(201).json({ link })
 
   } catch(e){
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'})
@@ -13,9 +40,11 @@ router.post('/generate', async(req,res) => {
 })
 
 // get. для получения всех ссылок
-router.get('/', async(req, res) => {
+router.get('/', auth, async(req, res) => {
   try{
-    const links = await Link.find({ owner: null}) //??
+    //const links = await Link.find({ owner: null}) //??
+    //запрос в базу где мы ищем все ссылки который относится к текущему пользователю
+    const links = await Link.find({ owner: req.user.userId })
     res.json(links)
   } catch(e){
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'})
@@ -23,7 +52,7 @@ router.get('/', async(req, res) => {
 })
 
 // get для получения ссылки по id
-router.get('/:id', async(req,res) => {
+router.get('/:id', auth,async(req,res) => {
   try{
     const link = await Link.findById(req.params.id) //??
     res.json(link)
