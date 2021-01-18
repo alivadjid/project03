ead3111253dec6486b8ca67a07bee271454f46cf
alivadjid1 <alivadjid@gmail.com>
2021-01-18 18:43:47 +0300

    добавление переменных на сервере. Добавление еще middleware

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
 //middlewar для корректного парсинга из фронта
 app.use(express.json({extended: true}))
 
+//формирование api
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV ==='production') {
+  //новый middleware
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  //любой другой запрос
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+  })
+}
 
 const PORT = config.get('port') || 5000
 
